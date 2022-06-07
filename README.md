1c8e966587f7f98680eec01ddd38cd90aa8b67aa
Brett Curtis <brett@osinfra.io>
2022-06-07 19:01:19 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# local-development-setup
-Development environment setup scripts for infrastructure as code (IaC)
+# Development environment setup scripts for infrastructure as code (IaC)
+
+## Ubuntu Setup
+
+To install all the local tools we will need you can run the following commands.
+
+*The following step is optional but will allow for sudo access without entering a password.*
+
+```none
+ echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo EDITOR='tee -a' visudo
+ ```
+
+```none
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/osinfra-io/local-development-setup/main/ubuntu/setup.sh)"
+```
+
+Change your default shell to Zsh and exit.
+
+```none
+chsh -s /home/linuxbrew/.linuxbrew/bin/zsh; exit
+```
+
+When you start Windows Terminal you will be prompted to set up Powerlevel10k. Choose the options you like and go!
+
+Once complete you can stay up to date by running the generated update script.
+
+```none
+~/bin/update.sh
+```
---
+++ ubuntu/setup.sh
@@ -0,0 +1,149 @@
+#!/usr/bin/env zsh
+
+# Ubuntu
+sudo apt update
+sudo apt -y upgrade
+sudo apt -y install build-essential apt-transport-https
+
+# Homebrew
+yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# Tools
+brew tap bridgecrewio/tap
+
+tools=(
+    awscli
+    azure-cli
+    bridgecrewio/tap/yor
+    checkov
+    gcc
+    gcc@5
+    gh
+    go
+    hashicorp/tap/terraform
+    helm
+    kubectl
+    kubectx
+    k9s
+    terraform-docs
+    pre-commit
+    romkatv/powerlevel10k/powerlevel10k
+    ruby
+    vault
+    zsh
+    zsh-syntax-highlighting
+)
+
+for tool in "${tools[@]}"
+do
+    brew install ${tool}
+done
+
+# Automatically enable pre-commit on repositories
+git config --global init.templateDir ~/.git-template
+pre-commit init-templatedir ~/.git-template
+
+# Zsh
+command -v zsh | sudo tee -a /etc/shells
+
+# Ruby Tools
+export RUBYOPT="-W:no-deprecated -W:no-experimental"
+
+echo 'gem: --no-document' > ~/.gemrc
+cat << EOF > ~/Gemfile
+source 'https://rubygems.org'
+
+gem 'kitchen-terraform'
+gem 'inspec'
+gem 'rubocop'
+gem 'cookstyle'
+EOF
+
+export BUNDLE_GEMFILE="~/Gemfile"
+
+cat << EOF > ~/.rubocop.yml
+Style/FrozenStringLiteralComment:
+          Enabled: false
+EOF
+
+gem install bundle
+gem update
+bundle update
+
+# Pathogen.vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Pathogen Plugins
+cd ~/.vim/bundle
+git clone https://github.com/hashivim/vim-terraform.git
+
+cat << EOF > ~/.vimrc
+set visualbell
+  
+execute pathogen#infect()
+
+filetype plugin indent on
+syntax on
+
+let g:terraform_fmt_on_save=1
+let g:terraform_align=1
+EOF
+
+# Create Update Script
+mkdir -p ~/bin
+cat << EOF > ~/bin/update.sh
+#!/usr/bin/env zsh
+
+source ~/.zshrc
+
+# Ruby
+gem update --system
+gem update
+gem cleanup
+bundle update
+
+# Ubuntu
+sudo apt update
+sudo apt -y upgrade
+sudo apt -y autoremove
+
+# Brew
+brew update
+brew upgrade
+brew autoremove
+brew cleanup
+
+# Pathogen Plugins
+cd ~/.vim/bundle/vim-terraform
+git pull
+
+# Oh-my-zsh
+omz update
+EOF
+
+chmod 755 ~/bin/update.sh
+
+# Google Cloud SDK
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" \
+| sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt update && sudo apt -y install google-cloud-sdk
+
+# Oh My Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Shell Setup
+cp .zshrc .zshrc-`date +"%Y%m%d_%H%M%S"`.bak
+echo 'alias gpg-passphrase="echo "test" | gpg --clearsign > /dev/null 2>&1"' >> ~/.zshrc
+echo 'source /home/linuxbrew/.linuxbrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ~/.zshrc
+echo 'source /home/linuxbrew/.linuxbrew/opt/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+echo 'export GOOGLE_AUTH_SUPPRESS_CREDENTIALS_WARNINGS=true' >> ~/.zshrc
+echo 'export GPG_TTY=$TTY' >> ~/.zshrc
+echo 'export EDITOR=vim' >> ~/.zshrc
+echo 'export RUBYOPT="-W:no-deprecated -W:no-experimental"' >> ~/.zshrc
+echo 'export BUNDLE_GEMFILE=$HOME/Gemfile' >> ~/.zshrc
+
+echo -e "eval \"\$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)\"\n$(cat ~/.zshrc)" > ~/.zshrc
+echo -e "export PATH=\$HOME/bin:/home/linuxbrew/.linuxbrew/lib/ruby/gems/3.1.0/bin:\$PATH\n$(cat ~/.zshrc)" > ~/.zshrc
