55f2b72f75b4dfc2590d87ace7f644b9f8b632dc
Brett Curtis <brett@osinfra.io>
2024-04-24 20:35:36 -0400

    Add gke-gcloud-auth-plugin to support changes to kubectl authentication in GKE v1.26

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -77,7 +77,7 @@ EOF
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" \
 | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-sudo apt update && sudo apt -y install google-cloud-sdk
+sudo apt update && sudo apt -y install google-cloud-sdk google-cloud-sdk-gke-gcloud-auth-plugin
 
 # Oh My Zsh
 
